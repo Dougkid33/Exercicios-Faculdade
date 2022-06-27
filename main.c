03f2475f9b7a78ef3cd2e161cf3d2f800674cb28
Doug Marcelino <105609994+Dougkid33@users.noreply.github.com>
2022-06-26 23:15:42 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    //criação e inicialização de vars
+    char frase[70];
+    int i;
+
+
+    printf("\n Insira a frase desejada...: ");
+    fflush(stdin);
+    gets(frase);
+
+    //desenvolvimento
+    for(i =0; frase != '\0'; i++);
+        {
+
+        }
+
+
+
+    printf(" %s .",frase);
+
+
+
+    printf("\n\n Hello world!\n");
+    return 0;
+}
